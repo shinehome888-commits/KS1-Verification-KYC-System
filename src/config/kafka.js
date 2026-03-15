f5df1238a072a5449048e420d46f6393dbd44e1c
KS1 <shinehome888@gmail.com>
2026-03-15 04:31:28 -0700

    Delete src directory

--- src/config/kafka.js
+++
@@ -1,8 +0,0 @@
-// Kafka is disabled until Upstash is configured
-const dummyProducer = {
-  send: async () => {},
-  connect: async () => {},
-  disconnect: async () => {}
-};
-
-module.exports = { producer: dummyProducer };
