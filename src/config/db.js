8db9d1f4703d834579e3bddce229030f49497192
KS1 <shinehome888@gmail.com>
2026-03-17 08:45:20 -0700

    src directory

--- src/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log('✅ MongoDB connected – KS1 KYC System');
-};
-
-module.exports = connectDB();
